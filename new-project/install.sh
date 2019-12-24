915f446afda00b5aee3cebe3b451531fe7b5edee
Sean Dietrich <sean@kanopistudios.com>
2019-12-24 13:59:56 -0800

    Fixing variables

--- new-project/install.sh
+++ new-project/install.sh
@@ -3,4 +3,8 @@
 # Install Docksal
 curl -fsSL get.docksal.io | bash
 
-fin config set --global DOCKSAL_VHOST_PROXY_IP=0.0.0.0 DOCKSAL_DNS_DOMAIN="[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
+# Set VHOST Proxy IP and the DNS Domain
+fin config set --global DOCKSAL_VHOST_PROXY_IP=0.0.0.0 DOCKSAL_DNS_DOMAIN="[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com"
+
+# Reset All Items After
+fin system reset
