17fe8d0cf4c80ed397f3245591881744b2e61a5c
Sean Dietrich <sean@kanopistudios.com>
2019-12-24 13:48:43 -0800

    Testing out configuration for katacoda

--- new-project/install.sh
+++ new-project/install.sh
@@ -2,3 +2,5 @@
 
 # Install Docksal
 curl -fsSL get.docksal.io | bash
+
+fin config set --global DOCKSAL_VHOST_PROXY_IP=0.0.0.0 DOCKSAL_DNS_DOMAIN="[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].[[KATACODA_DOMAIN]]"
